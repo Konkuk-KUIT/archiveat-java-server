a1f71aa581171cc56419f4518e540fe4239de71f
hyungyu-02 <yona20020209@gmail.com>
2026-01-28 18:06:13 +0900

    feat: edit nickname 기능 추가

---
+++ src/main/java/com/archiveat/server/domain/user/dto/request/NicknameRequest.java
@@ -0,0 +1,15 @@
+package com.archiveat.server.domain.user.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class NicknameRequest {
+    @NotBlank
+    private String nickname;
+}
