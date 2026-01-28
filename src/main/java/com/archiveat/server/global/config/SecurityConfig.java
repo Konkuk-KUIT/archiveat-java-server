6af2d06af6aeaf2735d627a2b767a529cadfbd95
hyungyu-02 <yona20020209@gmail.com>
2026-01-28 18:05:13 +0900

    feat: create JwtAuthenticationFilter

--- src/main/java/com/archiveat/server/global/config/SecurityConfig.java
+++ src/main/java/com/archiveat/server/global/config/SecurityConfig.java
@@ -1,23 +1,38 @@
 package com.archiveat.server.global.config;
 
+import com.archiveat.server.global.jwt.JwtAuthenticationFilter;
+import com.archiveat.server.global.jwt.JwtUtil;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtUtil jwtUtil;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(csrf -> csrf.disable())          // REST API면 보통 disable
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
                 .formLogin(form -> form.disable())     // 기본 /login 페이지 끄기
                 .httpBasic(basic -> basic.disable())   // Basic 인증 요구 끄기
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll() // signup/login/emailcheck 열기
+                        .requestMatchers("/user/**").authenticated() // 온보딩 관련 /user 경로는 인증 필요
                         .anyRequest().permitAll()               // 일단 전체 오픈(개발용)
                 )
+                .addFilterBefore(new JwtAuthenticationFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 }
