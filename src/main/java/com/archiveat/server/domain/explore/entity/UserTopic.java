adb2b224489b6544acee249a229c9c34935fd24f
hyungyu-02 <yona20020209@gmail.com>
2026-01-30 14:52:04 +0900

    fix: now, future 살림

--- src/main/java/com/archiveat/server/domain/explore/entity/UserTopic.java
+++ src/main/java/com/archiveat/server/domain/explore/entity/UserTopic.java
@@ -1,6 +1,7 @@
 package com.archiveat.server.domain.explore.entity;
 
 import com.archiveat.server.domain.user.entity.User;
+import com.archiveat.server.global.common.constant.PerspectiveType;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -23,13 +24,17 @@ public class UserTopic {
     @JoinColumn(name = "topic_id")
     private Topic topic;
 
+    @Enumerated(EnumType.STRING)
+    private PerspectiveType perspectiveType;
+
     /**
-     * 서비스 레이어에서 UserTopic 객체를 생성하기 위한 생성자입니다.
      * @param user 관심사를 등록하는 사용자
      * @param topic 사용자가 선택한 관심 토픽
+     * @param perspectiveType NOW(선택됨) 또는 FUTURE(선택되지 않음)
      */
-    public UserTopic(User user, Topic topic) {
+    public UserTopic(User user, Topic topic, PerspectiveType perspectiveType) {
         this.user = user;
         this.topic = topic;
+        this.perspectiveType = perspectiveType;
     }
 }
