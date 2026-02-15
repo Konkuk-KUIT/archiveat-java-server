440d9505946265d857feb374ecc3b39f243c01a9
이준용 <1dlwnsdyd1@gmail.com>
2026-02-15 14:16:46 +0900

    fix: 502

--- src/main/java/com/archiveat/server/global/config/DataMigrationRunner.java
+++ src/main/java/com/archiveat/server/global/config/DataMigrationRunner.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
-@Component
+// @Component
 @RequiredArgsConstructor
 public class DataMigrationRunner implements CommandLineRunner {
 
